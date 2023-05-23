8c7f9f3378bdda8dd6267b448306923ba1a41c1c
vidaibao <vutrungtin@gmail.com>
2023-05-24 00:06:53 +0900

    Add GUI, change BoardLogic to .Net Framework

---
+++ TicTacToe_Sluiter/BoardLogic01/Board.cs
@@ -0,0 +1,68 @@
+﻿namespace BoardLogic01
+{
+    public class Board
+    {
+        public int[] Grid { get; set; }
+        public Board() 
+        {
+            // initialize the Grid
+            Grid = new int[9];
+            for (int i = 0; i < 9; i++)     Grid[i] = 0;
+            
+        }
+
+
+        // return 0 if nobody win, return number player win, return -1 if no win and full square
+        public int Check4Winner()
+        {
+            // row 1
+            if (Grid[0] == Grid[1] && Grid[1] == Grid[2] && Grid[0] != 0)
+            {
+                return Grid[0];
+            }
+            // row 2
+            if (Grid[3] == Grid[4] && Grid[4] == Grid[5] && Grid[3] != 0)
+            {
+                return Grid[3];
+            }
+            // row 3
+            if (Grid[6] == Grid[7] && Grid[7] == Grid[8] && Grid[6] != 0)
+            {
+                return Grid[6];
+            }
+            // col 1
+            if (Grid[0] == Grid[3] && Grid[3] == Grid[6] && Grid[0] != 0)
+            {
+                return Grid[0];
+            }
+            // col 2
+            if (Grid[1] == Grid[4] && Grid[4] == Grid[7] && Grid[1] != 0)
+            {
+                return Grid[1];
+            }
+            // col 3
+            if (Grid[2] == Grid[5] && Grid[5] == Grid[8] && Grid[2] != 0)
+            {
+                return Grid[2];
+            }
+            // dia 1
+            if (Grid[0] == Grid[4] && Grid[4] == Grid[8] && Grid[0] != 0)
+            {
+                return Grid[0];
+            }
+            // dia 2
+            if (Grid[2] == Grid[4] && Grid[4] == Grid[6] && Grid[2] != 0)
+            {
+                return Grid[2];
+            }
+
+
+            // more game to play confirm
+            // 
+            //if (ConfirmGame() == 0)
+            //    return -1;
+            return 0;
+        }
+
+    }
+}
---
+++ TicTacToe_Sluiter/TicTacToeGUI/Form1.cs
@@ -0,0 +1,87 @@
+﻿using BoardLogic01;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TicTacToeGUI
+{
+    public partial class Form1 : Form
+    {
+
+        Board game = new Board();
+        Button[] buttons = new Button[9];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            game = new Board();
+
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+            buttons[6] = button7;
+            buttons[7] = button8;
+            buttons[8] = button9;
+
+            // add a common click event to each button
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Click += handleButtonClick;// gom func
+                buttons[i].Tag = i;
+            }
+
+        }
+
+        private void handleButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            MessageBox.Show($"button {clickedButton.Tag} was clicked.");
+
+            int gameSquareNumber = (int)clickedButton.Tag;
+            game.Grid[gameSquareNumber] = 1;
+
+            UpdateBoard();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateBoard();
+        }
+
+        private void UpdateBoard()
+        {
+            // assign an X or O to the text of each button based on the values in Board
+            for (int i = 0; i < game.Grid.Length; i++)
+            {
+                if (game.Grid[i] == 0)
+                {
+                    buttons[i].Text = "";
+                }
+                else if (game.Grid[i] == 1)
+                {
+                    buttons[i].Text = "X";
+                }
+                else if (game.Grid[i] == 2)
+                {
+                    buttons[i].Text = "O";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
